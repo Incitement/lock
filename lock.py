589597ab09d430a3336804e617c955a193f34735
Conner <iplaykk@gmail.com>
2022-11-26 11:23:11 -0700

    add rot13 and start of caesar to lock

--- lock.py
+++ lock.py
@@ -5,20 +5,40 @@ string_to_encode = b""
 encode_flag = b""
 
 #Base64 encode 
-def encode_base64():
-    base64_string = base64.b64encode(string_to_encode)
+def encode_base64(string_to_encode, encode_flag):
+    base64_msg = base64.b64encode(string_to_encode)
     print("Here is your encoded string:\n ")
-    print(base64_string)
+    print(base64_msg)
 
 #Base64 decode 
 def decode_base64():
-    base64_string_dec = base64.decode("ascii")
+    base64_msg_dec = base64.b64decode("ascii")
     print("Here is your decoded string:\n ")
-    print(base64_string_dec)
+    print(base64_msg_dec)
 
 
 #Caesar cipher encode 
-def encode_caesar():
+def encode_caesar(string_to_encode):
+    ABC = '0123456789abcdefghijklmnopqrstuvwxyz'
+    cc = ''
+    key = 3
+
+    for i in string_to_encode:
+        lower_i = i.lower()
+    
+    if lower_i in ABC:
+        cced = ''
+
+        if ABC.find(lower_i) > len(ABC) - (key + 1):
+            cced = ABC[(ABC.find(lower_i) + key) % len(ABC)]
+        else:
+            cced = ABC[ABC.find(lower_i) + key]
+
+        cc += ciphered.upper() if i.isupper() else cced
+
+    else:
+        cc += i
+    return cc
     print("Here is your encoded string:\n ")
     print()
 
@@ -29,7 +49,15 @@ def decode_caesar():
 
 
 #rot13 cipher encode 
-def encode_rot13():
+def encode_rot13(string_to_encode):
+    def lookup(x):
+        order, char = ord(x), x.lower()
+        if 'a' <= char <= 'm':
+            return chr(order + 13)
+        if 'n' <= char <= 'z':
+            return chr(order - 13)
+        return x
+    return ''.join(map(lookup, message))
     print("Here is your encoded string:\n ")
     print()
 
@@ -41,6 +69,6 @@ def decode_rot13():
 def main():
     string_to_encode = input("Please enter the string you wish to encode:\n")
     encode_flag = input("Please enter the cipher you wish to use:\n")
-
+    
 main()
-encode_base64()
+encode_caesar("test")

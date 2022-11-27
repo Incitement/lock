f39bd117cf8d0a4e9ea7f217f77086e538235f4d
Incitement <cdr@connerreed.com>
2022-11-26 18:32:18 -0700

    Removed caesar nonsense, fixed ROT13 encode

--- lock.py
+++ lock.py
@@ -12,33 +12,15 @@ def encode_base64(string_to_encode, encode_flag):
 
 #Base64 decode 
 def decode_base64():
-    base64_msg_dec = base64.b64decode("ascii")
+    
     print("Here is your decoded string:\n ")
-    print(base64_msg_dec)
+    print()
 
 
 #Caesar cipher encode 
 def encode_caesar(string_to_encode):
     ABC = '0123456789abcdefghijklmnopqrstuvwxyz'
-    cc = ''
-    key = 3
 
-    for i in string_to_encode:
-        lower_i = i.lower()
-    
-    if lower_i in ABC:
-        cced = ''
-
-        if ABC.find(lower_i) > len(ABC) - (key + 1):
-            cced = ABC[(ABC.find(lower_i) + key) % len(ABC)]
-        else:
-            cced = ABC[ABC.find(lower_i) + key]
-
-        cc += ciphered.upper() if i.isupper() else cced
-
-    else:
-        cc += i
-    return cc
     print("Here is your encoded string:\n ")
     print()
 
@@ -51,15 +33,14 @@ def decode_caesar():
 #rot13 cipher encode 
 def encode_rot13(string_to_encode):
     def lookup(x):
-        order, char = ord(x), x.lower()
+        order, char = ord(x), x.lower() 
         if 'a' <= char <= 'm':
             return chr(order + 13)
         if 'n' <= char <= 'z':
             return chr(order - 13)
         return x
-    return ''.join(map(lookup, message))
     print("Here is your encoded string:\n ")
-    print()
+    return ''.join(map(lookup, string_to_encode))
 
 #rot13 cipher decode
 def decode_rot13():

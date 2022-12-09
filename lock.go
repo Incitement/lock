1ea331e41d87a9071949f1bcf98174f88dc65d51
Incitement <cdr@connerreed.com>
2022-12-09 11:29:51 -0700

    oops

--- lock.go
+++ lock.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
-import base64 "encoding/base64"
+import (
+	base64 "encoding/base64"
+	"fmt"
+)
 
 func main() {
 	enctest := "teststring"
 
-	encodedString := base64.StdEncoding.EncodeToString([]byte(data))
+	encodedString := base64.StdEncoding.EncodeToString([]byte(enctest))
 	fmt.Println(encodedString)
 }
